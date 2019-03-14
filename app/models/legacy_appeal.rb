fdaf6321b04493e2d0ff482dbbe978990d5322ce
Moncef Belyamani <moncef@truss.works>
2019-03-14 11:40:57 -0400

    Remove disabled but valid Rubocop offenses
    **Why**: By disabling offenses we actually care about, such as
    complexity and class length, it allows classes or methods to get even
    bigger and more complex indefinitely.
    
    Now that we are using Code Climate, we can remove all of the
    `rubocop:disable` statements we care about because Code Climate will
    only complain about new offenses, not existing ones.

--- app/models/legacy_appeal.rb
+++ app/models/legacy_appeal.rb
@@ -6,7 +6,6 @@
 # The source of truth for legacy appeals is VACOLS, but legacy appeals may also be worked in Caseflow.
 # Legacy appeals have VACOLS and BGS as dependencies.
 
-# rubocop:disable Metrics/ClassLength
 class LegacyAppeal < ApplicationRecord
   include AppealConcern
   include AssociatedVacolsModel
@@ -991,4 +990,3 @@ class LegacyAppeal < ApplicationRecord
     end
   end
 end
-# rubocop:enable Metrics/ClassLength
